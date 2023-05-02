c407e44c63a9ee246fbab2a31cf73bf3cc0f191d
Rafael Correa <lumia.1986@outlook.com>
2023-05-02 09:22:29 -0300

    implementation with choose of  array user group in push action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component , OnInit} from '@angular/core';
+import { Component , ElementRef, OnInit, ViewChild} from '@angular/core';
 import { ManageActInacUsersService } from './manage-act-inac-users.service';
 
 @Component({
@@ -9,12 +9,29 @@ import { ManageActInacUsersService } from './manage-act-inac-users.service';
 export class AppComponent implements OnInit{  
   activeUsers!:string[];
   inactiveUsers!:string[];
-  
+  inputUser!: string;
+  @ViewChild('message') message!: ElementRef;
+ selectedValue!: string;
+
   constructor( private usersService: ManageActInacUsersService) {}
   
   ngOnInit(): void {
     this.activeUsers = this.usersService.activeUsers;
     this.inactiveUsers = this.usersService.inactiveUsers;
+    
+  }
+
+  sendUser( ){
+    
+  if (this.selectedValue === 'activeUsers' && this.activeUsers.includes(this.inputUser) === false){
+      this.activeUsers.push(this.inputUser);
+   } else if ( this.selectedValue === 'inactiveUsers'  && this.inactiveUsers.includes(this.inputUser) === false) {
+    this.inactiveUsers.push(this.inputUser);
+   } else {
+      console.log(this.message.nativeElement.id)
+      this.message.nativeElement.style.removeProperty('display:none');
+
+  }
 
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AcitveUsersComponentComponent } from './acitve-users-component/acitve-users-component.component';
 import { InacitveUsersComponentComponent } from './inacitve-users-component/inacitve-users-component.component';
 import { ManageActInacUsersService } from './manage-act-inac-users.service';
 import { CountsService } from './counts.service';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AcitveUsersComponentComponent,
-    InacitveUsersComponentComponent
+    InacitveUsersComponentComponent,
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [ManageActInacUsersService , CountsService],
   bootstrap: [AppComponent]
